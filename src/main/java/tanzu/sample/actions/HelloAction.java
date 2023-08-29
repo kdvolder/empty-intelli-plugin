23a02e9fcaef1f31cd45f0c8cd697603b76866b0
Kris De Volder <kdvolder@vmware.com>
2023-08-29 15:04:29 -0700

    initial commit

---
+++ src/main/java/tanzu/sample/actions/HelloAction.java
@@ -0,0 +1,12 @@
+package tanzu.sample.actions;
+
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.ui.messages.MessageDialog;
+import org.jetbrains.annotations.NotNull;
+
+public class HelloAction extends AnAction {
+    @Override
+    public void actionPerformed(@NotNull AnActionEvent e) {
+    }
+}
---
+++ src/test/java/com/vmware/SimpleTest.java
@@ -0,0 +1,15 @@
+package com.vmware;
+
+import com.intellij.testFramework.fixtures.BasePlatformTestCase;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class SimpleTest extends BasePlatformTestCase {
+
+
+    public void testSomething() {
+        Assert.assertNotNull(getProject());
+        Assert.fail("This should fail");
+    }
+
+}
